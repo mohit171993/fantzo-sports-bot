064822305d29b06d7cc84b556048a99152303bff
mohit171993 <mohit@interactivedigits.com>
2026-09-21 16:03:40 +0400

    Initialize IBETIN AI chat history storage

--- bot_persistent.py
+++ bot_persistent.py
@@ -25,6 +25,7 @@ import fantzo_analytics as analytics
 import fantzo_autoreply
 import fantzo_business
 import trial_live_tv
+import ibetin_ai
 
 logger = logging.getLogger(__name__)
 
@@ -295,6 +296,7 @@ def run() -> None:
     analytics.ensure_tables()
     fantzo_autoreply.ensure_setting()
     fantzo_business.ensure_tables()
+    ibetin_ai.ensure_tables()
 
     app = (
         Application.builder()
